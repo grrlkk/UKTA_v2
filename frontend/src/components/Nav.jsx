0824d3706d11c36a514d1b34f37c2136f8ac89e1
ttytu <quixote1103@gmail.com>
2024-01-03 22:54:03 +0900

    bg color and contents

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,19 +3,17 @@ import React from 'react';
 
 const Nav = () => {
 	return (
-		<nav className="bg-gray-800">
+		<nav className="bg-slate-50 fixed w-full">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
 					<div className="flex items-center">
-						<div className="flex-shrink-0">
-							<img className="h-8 w-8" src="/logo.svg" alt="Logo" />
-						</div>
+						<a href='#' className="flex-shrink-0 font-black">
+							KorCAT
+						</a>
 						<div className="hidden md:block">
 							<div className="ml-10 flex items-baseline space-x-4">
-								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
-								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+								<a href="#" className="text-slate-300 hover:text-slate-500 px-3 py-2">형태소</a>
+								<a href="#" className="text-slate-300 hover:text-slate-500 px-3 py-2">자질</a>
 							</div>
 						</div>
 					</div>
