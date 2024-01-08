ff984902c07e50511f7e86676db418398ec1b328
ttytu <quixote1103@gmail.com>
2024-01-08 15:57:42 +0900

    Footer formatting

--- frontend/src/components/Foot.jsx
+++ frontend/src/components/Foot.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Foot = () => {
 	return (
-		<footer className="bg-slate-50 text-black py-4 mt-10">
-			<div className="max-w-6xl mx-auto flex justify-between">
+		<footer className="bg-slate-50 text-black mt-10">
+			<div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-12">
 				<div>KDD Lab @ inha</div>
 				<a href="https://github.com/ttytu/KorCAT-web" className="text-slate-300 hover:text-slate-500">GitHub</a>
 			</div>
