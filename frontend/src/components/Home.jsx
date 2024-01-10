6b029c1e934ad33bbe48116ba95100a89633a2c1
ttytu <quixote1103@gmail.com>
2024-01-10 12:15:09 +0900

    init Homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 
 
-const Home = () => {
+const Home = ({ currentPage }) => {
 	const [inputValue, setInputValue] = useState('');
+	const [selectedFile, setSelectedFile] = useState(null);
 
 	const handleInputChange = (e) => {
 		setInputValue(e.target.value);
 	};
 
+	const handleFileInputChange = (e) => {
+		const file = e.target.files[0];
+		setSelectedFile(file);
+	};
+
 	return (
-		<div className="items-start pt-12 grid grid-cols-1 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-			<div className='mt-10 grid grid-cols-1'>
-				<h1 className="text-4xl font-bold py-2">First element</h1>
+		<div className="items-start pt-12 grid grid-cols-1 gap-16 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+			<h1 className="mt-10 text-4xl font-extrabold">{currentPage}</h1>
 
-				<textarea
-					value={inputValue}
-					onChange={handleInputChange}
-					className="px-4 py-2 border border-gray-300 rounded mb-4 overflow-scroll"
-					placeholder="Enter text"
-				/>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+				<a href='/morpheme' className='w-full aspect-[3/2] bg-slate-300 hover:bg-slate-500 hover:text-white font-extrabold px-4 py-2 rounded transition-all ease-in-out'>
+					<h2 className="text-3xl py-2">형태소 분석</h2>
+				</a>
 
-				<button className="px-4 py-2 bg-slate-500 text-white rounded hover:bg-slate-600">
-					Get Started
-				</button>
+				<a href='/morpheme' className='w-full aspect-[3/2] bg-slate-300 hover:bg-slate-500 hover:text-white font-extrabold px-4 py-2 rounded transition-all ease-in-out'>
+					<h2 className="text-3xl py-2">자질 분석</h2>
+				</a>
 			</div>
 		</div>
 	);
