8c27afd8a0dc03e5df3de1a43dc4267f327aaf89
ttytu <quixote1103@gmail.com>
2024-02-27 08:36:12 +0000

    updated page layout

--- frontend/src/components/ResultsCohesion.jsx
+++ frontend/src/components/ResultsCohesion.jsx
@@ -1,77 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 
-const dummy_cohes = [
-	{
-		"filename": "test.txt",
-		"contents": "This is a test file",
-		"results": [{
-			'lemmaCnt': 33,
-			'average_sentence_similarity': 0.32634028792381287,
-			'topic_consistency': 0.4278,
-			'conjuctions': 0.030303030303030304,
-			'lemmattr': 0.9696969696969697,
-			'lemmaMattr': -1,
-			'lexicalDensityTokens': 0.009105098855359001,
-			'lexicalDensityTypes': 0.00023649607416516885,
-			'contentTtr': 0.025974025974025976,
-			'functionTtr': 0.023172905525846704,
-			'nounTtr': 0.025846702317290554,
-			'verbTtr': 0.030303030303030304,
-			'adjTtr': 0,
-			'advTtr': 0.030303030303030304,
-			'bigramLemmaTtr': 1.0,
-			'trigramLemmaTtr': 1.0,
-			'adjacent_sentence_overlap_all_lemmas': 11,
-			'adjacent_sentence_overlap_all_lemmas_normed': 2,
-			'binary_adjacent_sentence_overlap_all_lemmas': 2,
-			'adjacent_two_sentence_overlap_all_lemmas': 4,
-			'adjacent_two_sentence_overlap_all_lemmas_normed': 1,
-			'binary_adjacent_two_sentence_overlap_all_lemmas': 1,
-			'adjacent_sentence_overlap_content_lemmas': 3,
-			'adjacent_sentence_overlap_content_lemmas_normed': 2,
-			'binary_adjacent_sentence_overlap_content_lemmas': 2,
-			'adjacent_two_sentence_overlap_content_lemmas': 1,
-			'adjacent_two_sentence_overlap_content_lemmas_normed': 1,
-			'binary_adjacent_two_sentence_overlap_content_lemmas': 1,
-			'adjacent_sentence_overlap_function_lemmas': 6,
-			'adjacent_sentence_overlap_function_lemmas_normed': 2,
-			'binary_adjacent_sentence_overlap_function_lemmas': 2,
-			'adjacent_two_sentence_overlap_function_lemmas': 2,
-			'adjacent_two_sentence_overlap_function_lemmas_normed': 1,
-			'binary_adjacent_two_sentence_overlap_function_lemmas': 0,
-			'adjacent_sentence_overlap_noun_lemmas': 4,
-			'adjacent_sentence_overlap_noun_lemmas_normed': 2,
-			'binary_adjacent_sentence_overlap_noun_lemmas': 2,
-			'adjacent_two_sentence_overlap_noun_lemmas': 2,
-			'adjacent_two_sentence_overlap_noun_lemmas_normed': 1,
-			'binary_adjacent_two_sentence_overlap_noun_lemmas': 1,
-			'adjacent_sentence_overlap_verb_lemmas': 1,
-			'adjacent_sentence_overlap_verb_lemmas_normed': 1,
-			'binary_adjacent_sentence_overlap_verb_lemmas': 0,
-			'adjacent_two_sentence_overlap_verb_lemmas': 0,
-			'adjacent_two_sentence_overlap_verb_lemmas_normed': 0,
-			'binary_adjacent_two_sentence_overlap_verb_lemmas': 0,
-			'adjacent_sentence_overlap_adjective_lemmas': 0,
-			'adjacent_sentence_overlap_adjective_lemmas_normed': 0,
-			'binary_adjacent_sentence_overlap_adjective_lemmas': 0,
-			'adjacent_two_sentence_overlap_adjective_lemmas': 0,
-			'adjacent_two_sentence_overlap_adjective_lemmas_normed': 0,
-			'binary_adjacent_two_sentence_overlap_adjective_lemmas': 0,
-			'adjacent_sentence_overlap_adverb_lemmas': 0,
-			'adjacent_sentence_overlap_adverb_lemmas_normed': 0,
-			'binary_adjacent_sentence_overlap_adverb_lemmas': 0,
-			'adjacent_two_sentence_overlap_adverb_lemmas': 0,
-			'adjacent_two_sentence_overlap_adverb_lemmas_normed': 0,
-			'binary_adjacent_two_sentence_overlap_adverb_lemmas': 0
-		}]
-	},
-]
-
-
 const ResultsCoh = () => {
 	const [cohesionResult, setCohesionResult] = useState([]);
-	const [selectedFile, setSelectedFile] = useState(-1);
+	const [selectedFile, setSelectedFile] = useState(0);
+	const [selectedProperty, setSelectedProperty] = useState([]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -80,16 +13,37 @@ const ResultsCoh = () => {
 				const data = await response.json();
 				setCohesionResult(data);
 				console.log(data);
+				for (let i = 0; i < data.length; i++) {
+					selectedProperty.push([]);
+				}
 			} catch (error) {
 				console.error(error);
 			}
 		};
 		fetchData();
-		console.log(cohesionResult);
-	}, []);
+	}, [selectedProperty]);
 
-	const handleFileDownload = () => {
-		console.log('Download');
+	const handleFileDownload = (item, index, type) => {
+		if (type === 'csv') {
+			console.log('csv');
+			const csvData = selectedProperty[index].map(p => p.split('\t').join(',')).join('\n');
+			const blob = new Blob([csvData], { type: 'text/csv' });
+			const url = URL.createObjectURL(blob);
+			const link = document.createElement('a');
+			link.href = url;
+			link.download = `${item._id}_cohesion.csv`;
+			link.click();
+			link.remove();
+		} else {
+			const data = JSON.stringify(item);
+			const blob = new Blob([data], { type: 'application/json' });
+			const url = URL.createObjectURL(blob);
+			const link = document.createElement('a');
+			link.href = url;
+			link.download = `${item._id}_cohesion.json`;
+			link.click();
+			link.remove();
+		}
 	}
 
 	const handleSelectFile = (index) => {
@@ -99,32 +53,71 @@ const ResultsCoh = () => {
 		}
 	}
 
+	const handleSelectProperty = (property, selectedFile) => {
+		return () => {
+			console.log(property);
+			const updatedSelectedProperty = selectedProperty[selectedFile]?.includes(property)
+				? selectedProperty[selectedFile].filter(p => p !== property)
+				: [...(selectedProperty[selectedFile] || []), property];
+			setSelectedProperty(prevSelectedProperty => {
+				const updatedProperty = [...prevSelectedProperty];
+				updatedProperty[selectedFile] = updatedSelectedProperty;
+				return updatedProperty;
+			});
+			console.log(selectedProperty);
+		};
+	};
+
+	const handleSelectAll = (results, index) => {
+		return () => {
+			if (selectedProperty[index].length === Object.entries(results).length) {
+				setSelectedProperty(prevSelectedProperty => {
+					const updatedProperty = [...prevSelectedProperty];
+					updatedProperty[index] = [];
+					return updatedProperty;
+				});
+			} else {
+				setSelectedProperty(prevSelectedProperty => {
+					const updatedProperty = [...prevSelectedProperty];
+					updatedProperty[index] = Object.entries(results).map(([key, value]) => key + '\t' + value);
+					return updatedProperty;
+				});
+			}
+		};
+	}
+
+
 	return (
 		<div className='grid grid-cols-1 gap-4 fade-in'>
 			<h2 className="text-3xl font-bold py-2">Cohesion Results</h2>
 
-			{cohesionResult.map((item, index) => (
-				<div key={index} className='p-2 border border-gray-300 rounded-lg overflow-auto w-full relative bg-slate-50'>
-					<div className='grid grid-cols-1 gap-4'>
-						<h3 className='text-xl font-bold'>{item.filename}</h3>
-						<p>{item.contents}</p>
-						<div key={index} className={`${selectedFile === index ? 'h-96 overflow-scroll' : 'h-0 overflow-hidden'} transition-all ease-in-out pr-2`}>
+			{cohesionResult.sort((a, b) => -a._id.localeCompare(b._id)).map((item, index) => (
+				<div key={index} className={`p-4 border border-gray-300 rounded-lg overflow-auto w-full relative transition-all ${selectedFile === index ? 'bg-slate-100' : 'bg-slate-50'}`}>
+					<div className='grid grid-cols-1'>
+						<div className='grid grid-cols-1 gap-4' onClick={handleSelectFile(index)}>
+							<h3 className='text-xl font-bold'>{item.filename}</h3>
+							<p>{item.contents}</p>
+						</div>
+
+						<div key={index} className={`${selectedFile === index ? 'h-96 mt-4 overflow-scroll' : 'h-0 overflow-hidden'} transition-all ease-in-out pr-2`}>
 							<table className='w-full'>
 								<thead>
 									<tr className='text-left border-b'>
-										<th className='p-1'></th>
+										<th className='p-1'>
+											<input type="checkbox" onChange={handleSelectAll(item.results, index)} />
+										</th>
 										<th className='p-1'>Property</th>
 										<th className='p-1'>Value</th>
 									</tr>
 								</thead>
 								<tbody>
 									{Object.entries(item.results).map(([key, value]) => (
-										<tr key={key} className='border-b'>
+										<tr key={key} className='border-b hover:bg-slate-200' onClick={handleSelectProperty(key + '\t' + value, index)}>
 											<td className='p-1'>
-												<input type="checkbox" id={key} name={key} value={key} />
+												<input type="checkbox" id={key} name={key} value={key} checked={selectedProperty[index]?.includes(key + '\t' + value)} onChange={() => { }} />
 											</td>
 											<td className='p-1'>
-												<label htmlFor={key}>{key}</label>
+												{key}
 											</td>
 											<td className='p-1'>
 												{value}
@@ -136,19 +129,24 @@ const ResultsCoh = () => {
 						</div>
 					</div>
 
-					<div className='absolute top-2 right-2 flex gap-2 text-sm'>
-						<button className={`grow sm:grow-0 px-4 py-2 bg-slate-500 text-white rounded-full hover:bg-slate-600 flex flex-nowrap gap-2`}>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-							</svg>
-							Download
-						</button>
+					<div className='absolute top-3 right-2 flex gap-2 text-sm'>
+						<div className='flex'>
+							<button className={`grow sm:grow-0 px-4 py-2 pr-2 bg-slate-500 text-white rounded-full rounded-r-none hover:bg-slate-600 flex flex-nowrap gap-2`} onClick={() => handleFileDownload(item)}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+								</svg>
+								json
+							</button>
+							<button className={`grow sm:grow-0 px-4 py-2 pl-2 bg-slate-500 text-white rounded-full rounded-l-none hover:bg-slate-600 flex flex-nowrap gap-2`} onClick={() => handleFileDownload(item, index, "csv")}>
+								csv
+							</button>
+						</div>
 
-						<button className={`grow sm:grow-0 p-2 rounded-full hover:bg-slate-200 transition-all ease-in-out ${selectedFile === index ? 'rotate-90' : ''}`} onClick={handleSelectFile(index)}>
+						<div className={`grow sm:grow-0 p-2 transition-all ease-in-out ${selectedFile === index ? '' : 'rotate-90'}`} onClick={handleSelectFile(index)}>
 							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
 								<path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 							</svg>
-						</button>
+						</div>
 					</div>
 				</div>
 			))}
--- frontend/src/components/ResultsMorpheme.jsx
+++ frontend/src/components/ResultsMorpheme.jsx
@@ -2,59 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Tags from './Tags';
 
 
-const dummy_morph = [
-	{
-		"filename": "test.txt",
-		"contents": "PSG 공격수인 음바페는 이번 여름 레알에 합류하기로 합의했다.",
-		"results": [
-			["PSG", "NNP"],
-			["공격수", "NNG"],
-			["이", "VCP"],
-			["ㄴ", "ETM"],
-			["음바페", "NNP"],
-			["는", "JX"],
-			["이번", "NNG"],
-			["여름", "NNG"],
-			["레알", "NNP"],
-			["에", "JKB"],
-			["합류하", "VV"],
-			["기", "ETN"],
-			["로", "JKB"],
-			["합의하", "VV"],
-			["았", "EP"],
-			["다", "EF"],
-			[".", "SF"]
-		]
-	},
-	{
-		"filename": "test2.txt",
-		"contents": "PSG 공격수인 음바페는 이번 여름 레알에 합류하기로 합의했다.",
-		"results": [
-			["PSG", "NNP"],
-			["공격수", "NNG"],
-			["이", "VCP"],
-			["ㄴ", "ETM"],
-			["음바페", "NNP"],
-			["는", "JX"],
-			["이번", "NNG"],
-			["여름", "NNG"],
-			["레알", "NNP"],
-			["에", "JKB"],
-			["합류하", "VV"],
-			["기", "ETN"],
-			["로", "JKB"],
-			["합의하", "VV"],
-			["았", "EP"],
-			["다", "EF"],
-			[".", "SF"]
-		]
-	},
-]
-
-
 const ResultsMor = () => {
 	const [morphemeResult, setMorphemeResult] = useState([]);
-	const [selectedFile, setSelectedFile] = useState(-1);
+	const [selectedFile, setSelectedFile] = useState(0);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -67,11 +17,17 @@ const ResultsMor = () => {
 			}
 		};
 		fetchData();
-		console.log(morphemeResult);
 	}, []);
 
-	const handleFileDownload = () => {
-		console.log('Download');
+	const handleFileDownload = (item) => {
+		const data = JSON.stringify(item);
+		const blob = new Blob([data], { type: 'application/json' });
+		const url = URL.createObjectURL(blob);
+		const link = document.createElement('a');
+		link.href = url;
+		link.download = `${item._id}_morpheme.json`;
+		link.click();
+		link.remove();
 	}
 
 	const handleSelectFile = (index) => {
@@ -85,47 +41,53 @@ const ResultsMor = () => {
 		<div className='grid grid-cols-1 gap-4 fade-in'>
 			<h2 className="text-3xl font-bold py-2">Morpheme Results</h2>
 
-			{morphemeResult.map((item, index) => (
-				<div key={index} className='p-2 border border-gray-300 rounded-lg overflow-auto w-full relative bg-slate-50'>
-					<div className='grid grid-cols-1 gap-4'>
-						<h3 className='text-xl font-bold'>{item.filename}</h3>
-						<p>{item.contents}</p>
-						<div className={`flex gap-3 overflow-scroll transition-all ease-in-out ${selectedFile === index ? "h-20" : "h-0 overflow-hidden"}`}>
-							{item.results.map((morph, index) => {
-								return (
-									<div key={index + morph} id={`${morph[1]}`} className={`group hover:bg-slate-200 relative text-sm font-bold flex-col whitespace-nowrap p-2 border-x-2 mb-2`}>
-										<div className='flex justify-center'>
-											{morph[0]}
+			<div className='grid grid-cols-1 gap-2'>
+				{morphemeResult.sort((a, b) => -a._id.localeCompare(b._id)).map((item, index) => (
+					<div key={index} className={`p-4 h-fit border border-gray-300 rounded-lg overflow-auto w-full relative transition-all ${selectedFile === index ? 'bg-slate-100' : 'bg-slate-50'}`}>
+						<div className={`grid grid-cols-1`}>
+							<div className='grid grid-cols-1 gap-4' onClick={handleSelectFile(index)}>
+								<h3 className='text-xl font-bold'>{item.filename}</h3>
+								<p>{item.contents}</p>
+							</div>
+
+							<div className={`flex gap-3 overflow-x-scroll overflow-y-hidden transition-all ease-in-out ${selectedFile === index ? "h-20 mt-4" : "h-0 overflow-hidden"}`}>
+								{item.results.map((morph, index) => {
+									return (
+										<div key={index + morph} id={`${morph[1]}`} className={`group hover:bg-slate-200 relative text-sm font-bold flex-col whitespace-nowrap p-2 border-x-2 mb-2`}>
+											<div className='flex justify-center'>
+												{morph[0]}
+											</div>
+											<div className='flex flex-col justify-center group-hover:gap-1 text-xs gap-1' style={{ "color": Tags.find(tag => tag.tag === morph[1])?.color }}>
+												<span className='flex justify-center'>
+													{morph[1]}
+												</span>
+												<span className='flex justify-center'>
+													{Tags.find(tag => tag.tag === morph[1])?.desc}
+												</span>
+											</div>
 										</div>
-										<div className='flex flex-col justify-center group-hover:gap-1 text-xs gap-1' style={{ "color": Tags.find(tag => tag.tag === morph[1])?.color }}>
-											<span className='flex justify-center'>
-												{morph[1]}
-											</span>
-											<span className='flex justify-center'>
-												{Tags.find(tag => tag.tag === morph[1])?.desc}
-											</span>
-										</div>
-									</div>
-								);
-							})}
+									);
+								})}
+							</div>
+						</div>
+
+						<div className='absolute top-3 right-2 flex gap-2 text-sm'>
+							<button className={`grow sm:grow-0 px-4 py-2 bg-slate-500 text-white rounded-full hover:bg-slate-600 flex flex-nowrap gap-2`} onClick={() => handleFileDownload(item)}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+								</svg>
+								json
+							</button>
+
+							<div className={`grow sm:grow-0 p-2 transition-all ease-in-out ${selectedFile === index ? '' : 'rotate-90'}`} onClick={handleSelectFile(index)}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+									<path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+								</svg>
+							</div>
 						</div>
 					</div>
-
-					<div className='absolute top-2 right-2 flex gap-2 text-sm'>
-						<button className={`grow sm:grow-0 px-4 py-2 bg-slate-500 text-white rounded-full hover:bg-slate-600 flex flex-nowrap gap-2`}>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-							</svg>
-							Download
-						</button>
-						<button className={`grow sm:grow-0 p-2 rounded-full hover:bg-slate-200 transition-all ease-in-out ${selectedFile === index ? 'rotate-90' : ''}`} onClick={handleSelectFile(index)}>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-								<path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-							</svg>
-						</button>
-					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 };
