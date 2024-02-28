1e16963e015763433cf6b6904fa7a430c9116b00
ttytu <quixote1103@gmail.com>
2024-02-28 18:18:39 +0000

    added foot logo

--- frontend/src/components/Foot.jsx
+++ frontend/src/components/Foot.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 
 const Foot = () => {
+	const logoText = `
+██╗  ██╗██████╗ ██████╗     ██╗      █████╗ ██████╗ 
+██║ ██╔╝██╔══██╗██╔══██╗    ██║     ██╔══██╗██╔══██╗
+█████╔╝ ██║  ██║██║  ██║    ██║     ███████║██████╔╝
+██╔═██╗ ██║  ██║██║  ██║    ██║     ██╔══██║██╔══██╗
+██║  ██╗██████╔╝██████╔╝    ███████╗██║  ██║██████╔╝
+╚═╝  ╚═╝╚═════╝ ╚═════╝     ╚══════╝╚═╝  ╚═╝╚═════╝ 
+`
+
 	return (
-		<footer className="bg-slate-50 mt-32">
-			<div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-12">
-				<div>KDD Lab @ inha</div>
-				<a href="https://github.com/ttytu/KorCAT-web" className="text-slate-300 hover:text-slate-500">GitHub</a>
+		<footer className="bg-slate-50 mt-32 py-4 text-[#677389]">
+			<div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-end">
+				<pre className='leading-none'>
+					{logoText}
+				</pre>
+
+				<p>KDD Lab @ INHA Univ.</p>
 			</div>
 		</footer>
 	);
