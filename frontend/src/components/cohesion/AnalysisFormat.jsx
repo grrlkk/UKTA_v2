6380b7c60df9adea216178106802cfa563274fe8
ttytu <quixote1103@gmail.com>
2025-02-28 09:56:02 +0000

    ngram words, chars list

--- frontend/src/components/cohesion/AnalysisFormat.jsx
+++ frontend/src/components/cohesion/AnalysisFormat.jsx
@@ -406,6 +406,196 @@ const ResultsList = ({ result, title }) => {
 	);
 };
 
+const ResultsListNgram = ({ result, title }) => {
+	const [selectedAll, setSelectedAll] = useState(false);
+	const [selectedProperty, setSelectedProperty] = useState([]);
+	const [hidden, setHidden] = useState(true);
+
+	const handleSelectProperty = (property) => {
+		return () => {
+			const updatedProperty = selectedProperty?.includes(property) ? selectedProperty.filter(p => p !== property) : [...selectedProperty, property];
+			setSelectedProperty(updatedProperty);
+			// console.log(updatedProperty);
+		};
+	};
+
+	const handleSelectAll = () => {
+		return () => {
+			if (selectedProperty.length === Object.entries(result).length) {
+				setSelectedProperty([]);
+				setSelectedAll(false);
+			} else {
+				setSelectedProperty(prevSelectedProperty => {
+					const updatedProperty = Object.entries(result).map((item, index) => index);
+					return updatedProperty;
+				});
+				setSelectedAll(true);
+				// console.log(selectedProperty);
+			}
+		};
+	};
+
+	const formatEntryTXT = (entry) => {
+		const type = entry[0];
+		const values = entry[1];
+		const str = Object.entries(values).map(([cnt, ngram]) => cnt + '\t' + ngram.join('\t')).join('\n');
+		return `${type}\n${str}\n`;
+	}
+
+	const formatEntryCSV = (entry) => {
+		const type = entry[0];
+		const values = entry[1];
+		const str = Object.entries(values).map(([cnt, ngram]) => type + ',' + cnt + ',' + ngram.join(',')).join('\n');
+		return `${str}`;
+	}
+
+	const handleFileDownload = (type) => {
+		if (type === 'csv') {
+			if (selectedProperty.length === 0) {
+				alert('선택된 자질이 없습니다.');
+				return;
+			}
+
+			const csvData = selectedProperty.map(p => formatEntryCSV(Object.entries(result)[p])).join('\n');
+			const BOM = "\uFEFF";
+			const blob = new Blob([BOM + csvData], { type: 'text/csv;charset=utf-8;' });
+			const url = URL.createObjectURL(blob);
+			const link = document.createElement('a');
+			link.href = url;
+			link.download = `${title}_selected.csv`;
+			link.click();
+			link.remove();
+		} else if (type === 'txt') {
+			if (selectedProperty.length === 0) {
+				alert('선택된 자질이 없습니다.');
+				return;
+			}
+
+			const csvData = selectedProperty.map(p => formatEntryTXT(Object.entries(result)[p])).join('\n');
+			const blob = new Blob([csvData], { type: 'text/plain' });
+			const url = URL.createObjectURL(blob);
+			const link = document.createElement('a');
+			link.href = url;
+			link.download = `${title}_selected.txt`;
+			link.click();
+			link.remove();
+		}
+	}
+
+	return (
+		<div className='text-sm overflow-hidden flex flex-col transition-all ease-in-out'>
+			<button onClick={() => setHidden(!hidden)} className={`btn-icon flex gap-2 items-center`}>
+				<h3 className='font-semibold'>{title}</h3>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`${!hidden && "rotate-90"} transition-transform ease-in-out w-5 h-5`}>
+					<path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+				</svg>
+			</button>
+
+			<div className={`${hidden ? "h-0 hidden" : "max-h-96 block pt-2"} rounded-xl overflow-hidden transition-all ease-in-out`}>
+				<div className='table-header p-2 rounded-t-xl overflow-hidden'>
+					<div className='flex items-center font-normal'>
+						<button className={`sm:grow-0 btn-primary py-1 px-3 rounded-lg rounded-r-none flex flex-nowrap gap-1 items-center`} onClick={() => handleFileDownload("txt")}>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+							</svg>
+							txt
+						</button>
+						<button className={`sm:grow-0 btn-primary py-1 px-3 rounded-lg rounded-l-none`} onClick={() => handleFileDownload("csv")}>
+							csv
+						</button>
+						<hr className="m-2 grow" />
+					</div>
+				</div>
+
+				<div className='table-contents w-full overflow-y-scroll max-h-72'>
+					<table className='w-full table-fixed'>
+						<thead className='w-full'>
+							<tr className='text-left'>
+								<th className='px-3 w-12 sticky top-0 table-header'>
+									<input type="checkbox" className='w-full accent-slate-600 align-middle' checked={selectedAll} onChange={handleSelectAll()} />
+								</th>
+								<th className='px-1 w-1/12 sticky top-0 table-header'>n.</th>
+								<th className='px-1 sticky top-0 table-header'>Tag ({selectedProperty.length})</th>
+								<th className='px-1 sticky top-0 table-header'>Target</th>
+								<th className='px-1 sticky top-0 table-header'>Description</th>
+								<th className='px-1 w-1/3 sticky top-0 table-header'>
+									<table>
+										<tbody>
+											<tr className=''>
+												<td className='p-1 w-12'>Cnt</td>
+												<td className='p-1'>
+													Ngram
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</th>
+							</tr>
+						</thead>
+						<tbody className='w-full'>
+							{Object.entries(result).map(([key, value], index) => (
+								<tr key={key} className='align-top' onClick={handleSelectProperty(index)}>
+									{console.log(value)}
+									<td className='p-1 px-3 w-12'>
+										<input
+											className='w-full accent-slate-600 align-middle'
+											type="checkbox" id={key} name={key} value={key} checked={selectedProperty.includes(index)} onChange={() => { }}
+										/>
+									</td>
+									<td className='p-1 w-1/12 font-mono italic'>
+										{Object.keys(result).indexOf(key) + 1}
+									</td>
+									<td className='p-1 break-all'>
+										{key}
+									</td>
+									<td className='p-1 break-all'>
+										<div className='flex flex-col'>
+											<span>
+												{MorphTags.find(tag => tag.tag === key.split("_")[0])?.desc}
+											</span>
+											<span>
+												{MorphTags.find(tag => tag.tag === key.split("_")[0])?.desc_eng}
+											</span>
+										</div>
+									</td>
+									<td className='p-1 break-all'>
+										<div className='flex flex-col'>
+											<span>
+												{CohTags[key.split("_")[1]]?.desc ||
+													CohTags[key]?.desc}
+											</span>
+											<span>
+												{CohTags[key.split("_")[1]]?.desc_eng ||
+													CohTags[key]?.desc_eng}
+											</span>
+										</div>
+									</td>
+									<td className='p-1 w-1/3 break-all'>
+										<table>
+											<tbody>
+												{Object.entries(value).map(([cnt, ngram]) => (
+													<tr key={ngram} className=''>
+														<td className='p-1 w-12'>{cnt}</td>
+														<td className='p-1 flex flex-row flex-wrap gap-1'>
+															{ngram.map((ng, idx) => (
+																<span key={idx} className='px-1 bg-slate-300 border-slate-400 border-2'>{ng}</span>
+															))}
+														</td>
+													</tr>
+												))}
+											</tbody>
+										</table>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 const GradeFormat = ({ results, title }) => {
 	const [hidden, setHidden] = useState(true);
 
@@ -476,4 +666,4 @@ const GradeFormat = ({ results, title }) => {
 
 
 
-export { CorrectionFormat, GradeFormat, MorphemeFormat, ResultsList, ResultsNumeric };
+export { CorrectionFormat, GradeFormat, MorphemeFormat, ResultsList, ResultsListNgram, ResultsNumeric };
--- frontend/src/components/cohesion/ResultsCohesion.jsx
+++ frontend/src/components/cohesion/ResultsCohesion.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 import OriginalText from '../OriginalText';
 import Pagination from '../Pagination';
-import { ResultsList, ResultsNumeric, MorphemeFormat, CorrectionFormat, GradeFormat } from './AnalysisFormat';
+import { ResultsList, ResultsListNgram, ResultsNumeric, MorphemeFormat, CorrectionFormat, GradeFormat } from './AnalysisFormat';
 import { EvalFormat } from './EvalFormat';
 import { useCompareFiles } from '../contexts/ComparisonContext';
 
@@ -58,6 +58,9 @@ const ResultCoh = ({ resultId, darkMode }) => {
 
 					<ResultsNumeric result={item.results.sentenceLvl} title={"Sentence/Paragraph Complexity"} />
 					<ResultsNumeric result={item.results.sentenceLvlRep} title={"Repetition, Standard Expression Per Sentence/Paragraph"} />
+					{item.results.sentenceLvlRep_list &&
+						<ResultsListNgram result={item.results.sentenceLvlRep_list} title={"Repetition, Standard Expression List"} />
+					}
 
 					<hr />
 
