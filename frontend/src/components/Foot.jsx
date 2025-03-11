1f1fe6ef7af595df879290481ad538f2de746d1e
ttytu <quixote1103@gmail.com>
2025-03-11 09:21:48 +0000

    footer credits update

--- frontend/src/components/Foot.jsx
+++ frontend/src/components/Foot.jsx
@@ -10,10 +10,10 @@ const Foot = () => {
 			<div className="max-w-6xl px-4 sm:px-6 lg:px-8 mx-auto flex flex-col lg:items-end lg:flex-row justify-between gap-16">
 				<div className='flex flex-col justify-between lg:justify-start items-center lg:items-start gap-4 w-full'>
 					<div className='divide-x-2 dark:divide-slate-700 *:px-2 flex justify-center'>
-						<Link to='/analysis' className='hover:font-bold first:pl-0'>Analysis</Link>
-						<Link to='/tagging' className='hover:font-bold'>Tag Information</Link>
+						<Link to='/analysis' className='hover:underling first:pl-0'>Analysis</Link>
+						<Link to='/tagging' className='hover:underline'>Tag Information</Link>
 					</div>
-					{/* <div className='flex flex-col'>
+					<div className='flex flex-col'>
 						<div className='flex flex-row gap-1'>
 							Developed by
 							<a href='https://sites.google.com/view/kdd-lab/' className='hover:text-slate-500 hover:underline font-bold'>
@@ -23,22 +23,22 @@ const Foot = () => {
 						<div className='flex flex-row gap-1'>
 							Official GitHub Repository
 							<a href='https://github.com/ttytu/KorCAT-web' className='hover:text-slate-500 hover:underline font-bold'>
-								KorCAT-web
+								UKTA-web
 							</a>
 						</div>
 						<div className='flex flex-row gap-1'>
-							Morpheme & Grammar Analysis by
+							Morpheme & Grammar Analysis Powered by
 							<a href='https://bareun.ai/' className='hover:text-slate-500 hover:underline font-bold'>
 								Bareun
 							</a>
 						</div>
-					</div> */}
+					</div>
 				</div>
 
-				{/* <div className='flex flex-row gap-4 grayscale w-full justify-center lg:justify-end dark:invert'>
+				<div className='flex flex-row gap-4 grayscale w-full justify-center lg:justify-end dark:invert'>
 					<img src={`${process.env.PUBLIC_URL}/kdd_logo_p.png`} alt="logo" className="h-8" />
 					<img src={`${process.env.PUBLIC_URL}/inha_logo_p.png`} alt="logo" className="h-8" />
-				</div> */}
+				</div>
 			</div>
 		</footer>
 	);
