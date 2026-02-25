5b67f342a4b3ecf76961798ca964e2c062c7f72b
grrlkk <zkxkck1112@gmail.com>
2026-02-25 07:04:14 +0000

    Fix: API Key

--- backend/apps/morph/bareun.py
+++ backend/apps/morph/bareun.py
@@ -1,38 +1,49 @@
 import ast
 import json
 import sys
+import os 
 
 import google.protobuf.text_format as tf
 import pandas as pd
 from bareunpy import Tagger
 from bareunpy import Corrector
 
-API_KEY = "koba-QUS4QWA-2ASEQVQ-U55HLPY-R2E5UOA"
-tagger = Tagger(API_KEY, "localhost", 5757)
-corrector = Corrector(API_KEY, "localhost", 5757)
+API_KEY_PATH = "/home/ukta/KorCAT-web_v2/bareun_api.txt"
 
+def load_api_key(path):
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read().strip()
+    except Exception as e:
+        print(f"API 키 파일을 읽는 중 오류 발생: {e}")
+        return ""
+
+API_KEY = load_api_key(API_KEY_PATH)
+
+tagger = Tagger(API_KEY, "localhost", 5656)
+corrector = Corrector(API_KEY, "localhost", 5656)
 
 class bareun:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def morphs(self, text):
-		return tagger.morphs(text)
+    def morphs(self, text):
+        return tagger.morphs(text)
 
-	def nouns(self, text):
-		return tagger.nouns(text)
+    def nouns(self, text):
+        return tagger.nouns(text)
 
-	def pos(self, text):
-		return tagger.pos(text)
-	
-	def tag(self, text):
-		return tagger.tag(text)
-	
-	def tags(self, text):
-		return tagger.tags(text)
-	
-	def correction(self, text):
-		return corrector.correct_error(content=text, auto_split=True)
-	
-	def corrections(self, text):
-		return corrector.correct_error_list(contents=text, auto_split=True)
+    def pos(self, text):
+        return tagger.pos(text)
+    
+    def tag(self, text):
+        return tagger.tag(text)
+    
+    def tags(self, text):
+        return tagger.tags(text)
+    
+    def correction(self, text):
+        return corrector.correct_error(content=text, auto_split=True)
+    
+    def corrections(self, text):
+        return corrector.correct_error_list(contents=text, auto_split=True)
